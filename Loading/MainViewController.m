31cc7bebc3ea969cfe335c965bab8e0a335bcb13
AdamEsgie <asalvitti.gucwa@gmail.com>
2014-01-01 18:53:05 -0500

    adding loading project - initial commit

---
+++ Loading/MainViewController.m
@@ -0,0 +1,67 @@
+
+#import "MainViewController.h"
+#import "LoadingView.h"
+
+@interface MainViewController ()
+
+@property (nonatomic, strong) LoadingView *loading;
+
+@end
+
+@implementation MainViewController
+
+- (id)init
+{
+  self = [super init];
+  if (self) {
+    self.view.backgroundColor = [UIColor whiteColor];
+  }
+  return self;
+}
+
+- (void)viewDidLoad
+{
+  [super viewDidLoad];
+  [self addButtons];
+  self.loading = [[LoadingView alloc] initWithCircleSize:6.0f];
+  [self.loading showInView:self.view withOrigin:CGPointMake(CGRectGetWidth(self.view.bounds)/2, CGRectGetHeight(self.view.bounds)/2)];
+}
+
+- (void)addButtons
+{
+  UIButton *startButton = [UIButton new];
+  startButton.frame = CGRectMake(CGRectGetWidth(self.view.bounds)/2-55, CGRectGetHeight(self.view.bounds)-120, 50, 40);
+  [startButton setTitle:@"Start" forState:UIControlStateNormal];
+  startButton.titleLabel.textColor = [UIColor blackColor];
+  startButton.backgroundColor = [UIColor lightGrayColor];
+  [startButton addTarget:self action:@selector(startAnimation) forControlEvents:UIControlEventTouchUpInside];
+  [self.view addSubview:startButton];
+  
+  UIButton *stopButton = [UIButton new];
+  stopButton.frame = CGRectMake(CGRectGetWidth(self.view.bounds)/2+55, CGRectGetHeight(self.view.bounds)-120, 50, 40);
+  [stopButton setTitle:@"Stop" forState:UIControlStateNormal];
+  stopButton.titleLabel.textColor = [UIColor blackColor];
+  stopButton.backgroundColor = [UIColor lightGrayColor];
+  [stopButton addTarget:self action:@selector(stopAnimation) forControlEvents:UIControlEventTouchUpInside];
+  [self.view addSubview:stopButton];
+  
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)startAnimation
+{
+  [self.loading startAnimating];
+}
+
+- (void)stopAnimation
+{
+  [self.loading stopAnimating];
+}
+
+
+@end
---
+++ Loading/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Loading
+//
+//  Created by dede on 12/31/13.
+//  Copyright (c) 2013 ESGIE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
